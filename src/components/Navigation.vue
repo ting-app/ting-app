1e67198635666e4badac03b61d38e16f81133b89
Frederick-S <mao_xiaodan@hotmail.com>
2022-07-05 10:42:27 +0800

    navigation: init

---
+++ src/components/Navigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <v-app-bar
+      absolute
+      color="white"
+    >
+      <v-spacer></v-spacer>
+      <v-btn text>
+        登陆
+      </v-btn>
+      <v-btn text>
+        注册
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data () {
+    return {}
+  }
+}
+</script>

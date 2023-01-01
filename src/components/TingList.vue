40907f87de1e57f174261f98d7320afbea4a4b37
Frederick-S <mao_xiaodan@hotmail.com>
2023-01-01 19:42:49 +0800

    get tings by page/pageSize

--- src/components/TingList.vue
+++ src/components/TingList.vue
@@ -1,53 +1,61 @@
 <template>
   <div>
     <Overlay :loading="loading"></Overlay>
-    <v-simple-table v-if="tings.length > 0" class="my-10">
-      <template v-slot:default>
-        <thead>
-        <tr>
-          <th class="text-center">标题</th>
-          <th class="text-center">描述</th>
-          <th class="text-center">听力文件</th>
-          <th class="text-center">创建时间</th>
-          <th class="text-center">更新时间</th>
-          <th class="text-center">操作</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="(ting, index) in tings" :key="ting.id">
-          <td class="text-center">{{ ting.title }}</td>
-          <td class="text-center">{{ ting.description }}</td>
-          <td class="text-center">{{ fileName(ting.audioUrl) }}</td>
-          <td class="text-center">{{ formatDateTime(ting.createdAt) }}</td>
-          <td class="text-center">{{ formatDateTime(ting.updatedAt) }}</td>
-          <td class="text-center">
-            <v-menu offset-y>
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  icon
-                  plain
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  <v-icon>mdi-cog</v-icon>
-                </v-btn>
-              </template>
-              <v-list>
-                <v-list-item class="pointer" @click="updateTing(ting)"
-                >
-                  <v-list-item-title>编辑</v-list-item-title>
-                </v-list-item>
-                <v-list-item class="pointer" @click="deleteTing(index, ting)"
-                >
-                  <v-list-item-title>删除</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-          </td>
-        </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+    <div v-if="tings.length > 0">
+      <v-simple-table class="my-10">
+        <template v-slot:default>
+          <thead>
+          <tr>
+            <th class="text-center">标题</th>
+            <th class="text-center">听力文件</th>
+            <th class="text-center">创建时间</th>
+            <th class="text-center">更新时间</th>
+            <th class="text-center">操作</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(ting, index) in tings" :key="ting.id">
+            <td class="text-center">{{ ting.title }}</td>
+            <td class="text-center">{{ fileName(ting.audioUrl) }}</td>
+            <td class="text-center">{{ formatDateTime(ting.createdAt) }}</td>
+            <td class="text-center">{{ formatDateTime(ting.updatedAt) }}</td>
+            <td class="text-center">
+              <v-menu offset-y>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    icon
+                    plain
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    <v-icon>mdi-cog</v-icon>
+                  </v-btn>
+                </template>
+                <v-list>
+                  <v-list-item class="pointer" @click="updateTing(ting)"
+                  >
+                    <v-list-item-title>编辑</v-list-item-title>
+                  </v-list-item>
+                  <v-list-item class="pointer" @click="deleteTing(index, ting)"
+                  >
+                    <v-list-item-title>删除</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+            </td>
+          </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+      <div class="text-center">
+        <v-pagination
+          v-model="page"
+          :length="totalPages"
+          :total-visible="5"
+          @input="selectPage"
+        ></v-pagination>
+      </div>
+    </div>
     <UpdateTing></UpdateTing>
   </div>
 </template>
@@ -60,6 +68,7 @@ import UnauthorizedError from '@/error/unauthorized-error'
 import UpdateTing from '@/components/UpdateTing.vue'
 import Overlay from '@/components/Overlay.vue'
 import { formatDateTime } from '@/util'
+import Constant from '@/constant'
 
 export default {
   name: 'TingList',
@@ -67,10 +76,18 @@ export default {
     Overlay,
     UpdateTing
   },
+  computed: {
+    totalPages () {
+      return Math.ceil(this.totalCount / this.pageSize)
+    }
+  },
   data () {
     return {
       loading: false,
-      tings: []
+      tings: [],
+      page: 1,
+      pageSize: Constant.defaultPageSize / 10,
+      totalCount: 0
     }
   },
   methods: {
@@ -118,18 +135,17 @@ export default {
         .finally(() => {
           this.loading = false
         })
-    }
-  },
-  created () {
-    eventBus.$on(EventTypes.TING_CREATED, this.tingCreated)
-    eventBus.$on(EventTypes.TING_UPDATED, this.tingUpdated)
+    },
+    selectPage (page) {
+      this.getTings(page)
+    },
+    getTings (page) {
+      const programId = this.$route.params.programId
 
-    const programId = this.$route.params.programId
-
-    if (programId) {
       this.loading = true
+      this.page = page
 
-      axios.get(`/tings?programId=${programId}`)
+      axios.get(`/tings?programId=${programId}&page=${this.page}&pageSize=${this.pageSize}`)
         .then((response) => {
           this.tings = response
         })
@@ -142,6 +158,30 @@ export default {
           this.loading = false
         })
     }
+  },
+  created () {
+    eventBus.$on(EventTypes.TING_CREATED, this.tingCreated)
+    eventBus.$on(EventTypes.TING_UPDATED, this.tingUpdated)
+
+    const programId = this.$route.params.programId
+
+    axios.get(`/tings:count?programId=${programId}`)
+      .then((response) => {
+        this.totalCount = response
+      })
+      .then(() => {
+        const page = 1
+
+        return this.getTings(page)
+      })
+      .catch((error) => {
+        console.error(error)
+
+        this.$toast.error(error.message)
+      })
+      .finally(() => {
+        this.loading = false
+      })
   }
 }
 </script>
